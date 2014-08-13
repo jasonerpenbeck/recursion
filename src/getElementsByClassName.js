cd6a6fcddae0ea26739bac4a639efb92dea0db99
Jason Erpenbeck <jason.erpenbeck@gmail.com>
2014-08-13 14:31:03 -0700

    Removed some of the messages to console.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,39 +3,23 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// use document.body, element.childNodes, and element.classList
-
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-    console.log('looking for: ' + className);
-
-
 
     var arrDOM = [];
 
     function searchDOM(startingPoint) {
-
-      console.log('My array with matches: ');
-      console.log(arrDOM);
-
-      console.log('Currently working through: ')
-      console.log(startingPoint);
-      console.log(startingPoint.childNodes);
 
       if(startingPoint.classList === undefined) {
         console.log("No Class List Here.  Move along");
       }
 
       else {
-        console.log("Look Through The Class List.");
 
         for(var i=0;i<startingPoint.classList.length;i++) {
-          console.log(i + ': Iterating Through classList');
-
 
           if(startingPoint.classList[i] == className) {
               arrDOM.push(startingPoint);
-              console.log('Match!');
           }
 
           else {
@@ -45,7 +29,6 @@ var getElementsByClassName = function(className){
         }
 
       }
-
 
       if(startingPoint.childNodes.length ===0) {
          console.log("Nothing further");
